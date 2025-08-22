3e6bf64d33164078c79150c74040fec9fd78ace1
fecarneiro <fe.iacarneiro@gmail.com>
2025-08-22 16:55:01 -0300

    Refactor createEmbedding function to encapsulate embedding creation logic

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ config();
 const openai = new OpenAI();
 openai.apiKey = process.env.OPENAI_API_KEY;
 
-const embedding = await openai.embeddings.create({
-  model: 'text-embedding-3-large',
-  input: 'taco de beisebol',
-  encoding_format: 'float',
-});
+async function createEmbedding() {
+  // 1.
 
-console.log(JSON.stringify(embedding.data));
+  const embedding = await openai.embeddings.create({
+    model: 'text-embedding-3-large',
+    input: 'taco de beisebol',
+    encoding_format: 'float',
+  });
+
+  console.log(JSON.stringify(embedding.data));
+}
