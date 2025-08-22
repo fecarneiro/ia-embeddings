9c01122c0357906f8bb3e9109ed4a2d1a32445d7
fecarneiro <fe.iacarneiro@gmail.com>
2025-08-22 17:08:17 -0300

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import OpenAI from 'openai';
 import { config } from 'dotenv';
+import fs from 'node:fs/promises';
 config();
 
 const openai = new OpenAI();
 openai.apiKey = process.env.OPENAI_API_KEY;
 
-async function createEmbedding() {
-  // 1.
-
-  const embedding = await openai.embeddings.create({
-    model: 'text-embedding-3-large',
-    input: 'taco de beisebol',
-    encoding_format: 'float',
-  });
-
-  console.log(JSON.stringify(embedding.data));
+async function createEmbedding(inputFile) {
+  try {
+    // 1. Receive input
+    const inputData = await fs.readFile(inputFile, 'utf-8');
+    // 2. Call openai embedding API
+    const embedding = await openai.embeddings.create({
+      model: 'text-embedding-3-large',
+      input: inputData,
+      encoding_format: 'float',
+    });
+    // 3. Save output on new file
+    await fs.writeFile('result.txt', JSON.stringify(embedding), 'utf-8');
+    console.log('Embedding generated successfully.');
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+createEmbedding('example.txt');
