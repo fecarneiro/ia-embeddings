7881ba59c2da2937606a379d7c2763d63a2f106c
fecarneiro <fe.iacarneiro@gmail.com>
2025-08-22 16:06:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import OpenAI from 'openai';
+import { config } from 'dotenv';
+config();
+
+const openai = new OpenAI();
+openai.apiKey = process.env.OPENAI_API_KEY;
+
+const embedding = await openai.embeddings.create({
+  model: 'text-embedding-3-large',
+  input: 'taco de beisebol',
+  encoding_format: 'float',
+});
+
+console.log(embedding);
